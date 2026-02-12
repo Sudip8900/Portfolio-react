cc3755774e39c9ba983a890e9d127c1c76c36461
Sudip8900 <iamsudippan@gmail.com>
2026-02-12 21:59:37 +0530

    Minor Changes

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [ tailwindcss(), react()],
-  base: "/Portfolio-react/",
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [tailwindcss(), react()],
+  base: mode === "production" ? "/Portfolio-react/" : "/",
+}))
